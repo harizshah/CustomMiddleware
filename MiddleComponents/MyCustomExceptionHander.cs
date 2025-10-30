fbd9a50c673ac545aabfe5b3add637992d063930
harizshah <harizsharizad@gmail.com>
2025-10-30 21:32:23 +0800

    done on custom middleware

---
+++ MiddleComponents/MyCustomExceptionHander.cs
@@ -0,0 +1,19 @@
+﻿namespace harizcustomemiddleware.MiddleComponents
+{
+    public class MyCustomExceptionHandler : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                context.Response.ContentType = "text/html";
+                await next(context);
+            }
+            catch (Exception ex) 
+            {   
+                await context.Response.WriteAsync("<h2>Error:</h2>");
+                await context.Response.WriteAsync($"<p>{ex.Message}</p>");
+            }
+        }
+    }
+}
---
+++ MiddleComponents/MyCustomMiddleware.cs
@@ -0,0 +1,18 @@
+﻿namespace harizcustomemiddleware.MiddleComponents;
+
+public class MyCustomMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public MyCustomMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        await context.Response.WriteAsync("MyCustomMiddleware: Before calling next\r\n");
+        await _next(context);
+        await context.Response.WriteAsync("MyCustomMiddleware: After calling next\r\n");
+    }
+}
